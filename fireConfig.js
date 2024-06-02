babf1bbfed23686d68290da51cdff0a2bc269ba2
cattalong <144977862+cattalong@users.noreply.github.com>
2024-06-03 03:22:55 +0800

    initial commit

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBZLpdBeFfj32uiM2NAjr8JYJ4e_F8Oeo8",
+  authDomain: "crud-mon-pm-f28e9.firebaseapp.com",
+  databaseURL: "https://crud-mon-pm-f28e9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "crud-mon-pm-f28e9",
+  storageBucket: "crud-mon-pm-f28e9.appspot.com",
+  messagingSenderId: "474120368143",
+  appId: "1:474120368143:web:a41dc2e74e0fa75c55b37a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
